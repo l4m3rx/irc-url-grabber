7baf9574a547679a620e51bef24944cc245ae172
Georgi Kolev <georgi.kolev@gmail.com>
2021-06-09 22:34:32 +0300

    Update conf.py

--- conf.py
+++ conf.py
@@ -13,7 +13,7 @@ REALNAME = 'Testis'
 QUIT_MSG = 'I quit!'
 # URL Grabber
 TITLE_TIMEOUT   = 15
-TITLE_PREFIX    = ircstyle.style("»", fg="green", reset=True) # ⤷
+TITLE_PREFIX    = ircstyle.style("⤷", fg="green", reset=True)
 DOMAIN_BACKLIST = [
     "127.0.0.1",
     "localhost",
